458fbf5587f5d9c62e95fb37702dfa1832e0cddd
WestEast1st <sai10nhid@gn.iwasaki.ac.jp>
2018-04-27 09:01:25 +0900

    APIのルート設定

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,36 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(["middleware" => "aip"],function(){
+    /*AllPageAPI*/
+    Route::get('headdata'   ,"UsersController@getHeadData"          );
+        /*this api is UserID & icon & point*/
+
+    /*MyPageAPI*/
+    Route::get('data',"UsersController@getUserData");
+        /*this api is gets icon & UserID*/
+    Route::get('solvelist'  ,"ActlogController@getMySolveListData"  );
+        /*this api is gets SUMPoint & Rank & SUMSolve*/
+    Route::get('mypoint'    ,"ActlogController@getMySolvePoint"      );
+        /*this api is gets SUMPoint & Rank & SUMSolve*/
+
+    /*Challenge(main)PageAPI*/
+    Route::get('topproblems',"ProblemController@getTopProblemsData" );
+        /*this api is gets SUMPoint & Rank & SUMSolve*/
+
+    /*ProblemPageAPI*/
+    Route::get('problem'    ,"ProblemController@getProblemData"     );
+        /*this api is problemdata & is_solve & solveNoP*/
+    Route::post('flagsolve' ,"ProblemController@postFlagSolve"      );
+        /*this api is post solve user icon */
+
+    /*ProblemPageAPI*/
+    Route::get('top10graph' ,"ActlogController@getTop10graphData"   );
+        /*this api is gets top10graph 30ms*/
+    Route::get('scores'     ,"ActlogController@getScoreData"        );
+        /*this api is gets */
+
+    Route::middleware('auth:api')->get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
