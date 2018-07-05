90f5fd0c4b2ab0dcd75e274d742b8685543e2bf5
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2018-07-05 20:54:36 +0900

    [add]problemの表示調整

--- resources/assets/js/components/Main.vue
+++ resources/assets/js/components/Main.vue
@@ -15,11 +15,42 @@
                 <a id="btn" class="col-lg-3">NET</a>
             </div>
         </div>
-        <div id="message">
-            <h1>競技形式</h1>
-            <p>Jeopardy</p>
-            <h1>競技時間</h1>
-            <p>XXXX - MM - DD hh:mm:ss ~ XXXX - MM - DD hh:mm:ss</p>
+        <div id="message" class="col-lg-12">
+            <div id="problem-zone" class="col-lg-offset-1 col-lg-10">
+
+                <div id="problem-tile" class="col-lg-3" >
+                    <router-link to="/main/problem" id="is-problem-solve">
+                        <div id="problem-info" >
+                            <div id="problem-title">
+                                <p><b>test-problem</b></p>
+                            </div>
+                            <div id="problem-category">
+                                <p><b>test</b></p>
+                            </div>
+                            <div id="problem-point">
+                                <p><b>999</b></p>
+                            </div>
+                        </div>
+                    </router-link>
+                </div>
+
+                <div id="problem-tile" class="col-lg-3" >
+                    <router-link to="/main/problem" id="problem">
+                        <div id="problem-info" >
+                            <div id="problem-title">
+                                <p><b>test-problem</b></p>
+                            </div>
+                            <div id="problem-category">
+                                <p><b>test</b></p>
+                            </div>
+                            <div id="problem-point">
+                                <p><b>999</b></p>
+                            </div>
+                        </div>
+                    </router-link>
+                </div>
+
+            </div>
         </div>
     </div>
 </div>
