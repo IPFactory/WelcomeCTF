bcae6ec3fccc864be38a66997b1364de2af61ef3
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2018-07-05 20:54:57 +0900

    [new]problem ページの雛形制作

--- resources/assets/js/components/Problem.vue
+++ resources/assets/js/components/Problem.vue
@@ -1,5 +1,17 @@
 <template>
-	<Header></Header>
+<div id="top">
+    <div class="container">
+        <div id="body_band">
+            <p id="problem-info">  <span>Web</span> <span>Q.1 test</span> <span>100pt</span> </p>
+        </div>
+        <div id="back-list">
+            <router-link to="/main"><b>&lt; &lt; problem::list</b></router-link >
+            </div>
+        <div id="message">
+        </div>
+
+    </div>
+</div>
 </template>
 
 <script>
