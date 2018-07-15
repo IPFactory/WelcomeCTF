1b8c20700c7b1d97413c3eed45691e045028575b
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2018-07-16 00:14:05 +0900

    [add]main(問題)ページの制作

--- resources/assets/js/components/Main.vue
+++ resources/assets/js/components/Main.vue
@@ -1,21 +1,21 @@
 <template>
-<div id="top">
+<div id="main">
     <div class="container">
         <div id="body_band">
-            <div class="col-lg-offset-2 col-lg-4">
-                <a id="btn" class="col-lg-3">ALL</a>
-                <a id="btn" class="col-lg-3">MISC</a>
-                <a id="btn" class="col-lg-3">PPC</a>
-                <a id="btn" class="col-lg-3">CRYPT</a>
+            <div class="col-lg-offset-2 col-md-offset-2 col-sm-offset-2 col-xs-offset-2 col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                <a id="btn" class="col-lg-3 col-md-3 col-sm-3 col-xs-3">ALL</a>
+                <a id="btn" class="col-lg-3 col-md-3 col-sm-3 col-xs-3">MISC</a>
+                <a id="btn" class="col-lg-3 col-md-3 col-sm-3 col-xs-3">PPC</a>
+                <a id="btn" class="col-lg-3 col-md-3 col-sm-3 col-xs-3">CRYPT</a>
             </div>
-            <div class="col-lg-4">
-                <a id="btn" class="col-lg-3">WEB</a>
-                <a id="btn" class="col-lg-3">BIN</a>
-                <a id="btn" class="col-lg-3">PWN</a>
-                <a id="btn" class="col-lg-3">NET</a>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                <a id="btn" class="col-lg-3 col-md-3 col-sm-3 col-xs-3">WEB</a>
+                <a id="btn" class="col-lg-3 col-md-3 col-sm-3 col-xs-3">BIN</a>
+                <a id="btn" class="col-lg-3 col-md-3 col-sm-3 col-xs-3">PWN</a>
+                <a id="btn" class="col-lg-3 col-md-3 col-sm-3 col-xs-3">NET</a>
             </div>
         </div>
-        <div id="message" class="col-lg-12">
+        <div id="message" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
             <div id="problem-zone" class="col-lg-offset-1 col-lg-10">
 
                 <div id="problem-tile" class="col-lg-3" >
@@ -55,3 +55,18 @@
     </div>
 </div>
 </template>
+<script>
+    export default {
+        props:["show"],
+        mounted () {
+            this.isAuth ()
+        },
+        methods :{
+            isAuth () {
+                if (!this.show) {
+                    this.$router.push('/')
+                }
+            }
+        },
+    }
+</script>
