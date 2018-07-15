b6117fdfc5eb8f3fa5a0cc5e1aa1a23a569b5ba8
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2018-07-16 00:05:22 +0900

    [add]認証コントローラー追加

--- app/Http/Controllers/Auth/AuthenticateController.php
+++ app/Http/Controllers/Auth/AuthenticateController.php
@@ -1,58 +1,49 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Auth;
 
-use Illuminate\Http\Request;
 use JWTAuth;
-use Tymon\JWTAuth\Exceptions\JWTException;
-use App\User;
+use Illuminate\Http\Request;
+
+use App\Model\User as User;
+use App\Http\Controllers\Controller;
 use App\Service\AuthServiceDigest as AuthServiceDigest;
 
 class AuthenticateController extends Controller
 {
     private $AuthService;
-    private $requestData;
 
-    public function __construct (Request $request)
-    {
-        $this->requestData      = $request;
-        $this->AuthService      = new AuthServiceDigest($request);
+    public function __construct (Request $request) {
+        $this->AuthService = new AuthServiceDigest($request);
     }
+
     public function registInDigest ()
     {
-        if (!$user   = $this->AuthService->userAccountRegist()) {
+        if (!$user = $this->AuthService->registPreparation()) {
             return response()->json(['error' => 'invalid_credentials'], 401);
         }
-        authenticate();
+        $token = $this->AuthService->authenticate();
+        return response()->json($token);
     }
+
+    public function loginInDigest ()
+    {
+        if (!$user = $this->AuthService->loginPreparation()) {
+            return response()->json(['error' => 'invalid_credentials'], 401);
+        }
+        $token = $this->AuthService->authenticate();
+        return response()->json($token);
+    }
+
     public function authenticateInTwitter ()
     {
-
+        //TODO twitterソーシャルログインの実装
     }
     public function authenticateInGithub ()
     {
-
+        //TODO githubソーシャルログインの実装
     }
-    public function authenticate ()
-    {
-        // grab credentials from the request
-        $credentials = $this->requestData->only('email', 'password');
 
-        try {
-            // attempt to verify the credentials and create a token for the user
-            if (! $token = JWTAuth::attempt($credentials)) {
-                return response()->json(['error' => 'invalid_credentials'], 401);
-            }
-        } catch (JWTException $e) {
-            // something went wrong whilst attempting to encode the token
-            return response()->json(['error' => 'could_not_create_token'], 500);
-        }
-
-        $user = User::where('email', $this->requestData->email)->first();
-
-        // all good so return the token
-        return response()->json(compact('user', 'token'));
-    }
 
     public function getCurrentUser()
     {
@@ -63,5 +54,6 @@ class AuthenticateController extends Controller
     public function logout()
     {
     }
+
 
 }
