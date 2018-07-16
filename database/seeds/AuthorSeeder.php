f8a8833e0abf4aa58ce4a96cb09e5bec121450cf
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2018-07-16 15:04:58 +0900

    [new]各種seeder関連

--- database/seeds/AuthorSeeder.php
+++ database/seeds/AuthorSeeder.php
@@ -1,8 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
-class DatabaseSeeder extends Seeder
+class AuthorSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,6 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UserTableSeeder::class);
+
     }
 }
