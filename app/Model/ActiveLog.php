d4265c234181e7482a555d2dc301acbbaf0d45a9
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2018-07-16 18:24:12 +0900

    [new]ActiveLogの刷新
    現状soleveのみ

---
+++ app/Model/ActiveLog.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Model;
+
+use App\Model\User;
+use App\Model\Problem;
+use Illuminate\Database\Eloquent\Model;
+
+class ActiveLog extends Model
+{
+    //act_log
+    protected $table = "ActiveLogs";
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id','user_id','problem_id','is_open','is_solve'
+    ];
+
+}
