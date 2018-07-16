e81e7098b397dc3c3cf4be1ab0cd7486124a2300
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2018-07-16 15:01:37 +0900

    [new] act_log用のSeeder＋検証用のdummyの皮

---
+++ database/seeds/ActiveLogSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ActiveLogSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
