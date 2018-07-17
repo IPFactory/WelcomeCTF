43c3200ae1df19954dfbf32acddfafcc04696436
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2018-07-17 19:47:31 +0900

    [add]問題用コントローラー

---
+++ app/Http/Controllers/Problem/ProblemController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Problem;
+
+use JWTAuth;
+use App\Model\User;
+use App\Model\Problem;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use App\Service\ProblemService as ProblemService;
+
+
+class ProblemController extends Controller
+{
+    private $user;
+    private $ProblemService;
+    private $id;
+
+    public function __construct (Request $Request) {
+        try {
+            $this->user           =   JWTAuth::parseToken()->authenticate();
+            $this->ProblemService =   new ProblemService($this->user->id, $Request);
+        } catch (JWTException $e){
+            return response()->json(['error' => 'Authentication failed'], 401);
+        }
+    }
+    /*
+    *   Control
+    */
+    public function getProblemInfo ($id) {
+        return response()->json($this->ProblemService->getInfo($id));
+    }
+
+    public function getProblemList () {
+        return response()->json($this->ProblemService->getList());
+    }
+
+
+}
