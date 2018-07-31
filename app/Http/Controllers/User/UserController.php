b21b7af2d8c16a8e7d45b91f5486c667d2117527
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2018-07-31 21:00:05 +0900

    [fix]コントローラーからロジックをpurge

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -5,44 +5,29 @@ namespace App\Http\Controllers\User;
 use JWTAuth;
 use Illuminate\Http\Request;
 use Tymon\JWTAuth\Exceptions\JWTException;
-
 use App\Http\Controllers\Controller;
-use App\Model\User as User;
-use App\Model\ActiveLog as ActiveLog;
-use App\Model\Problem as Problem;
+use App\Service\UserService as UserService;
 
 
 class UserController extends Controller
 {
-    protected $user;
+    private $user;
+    private $UserService;
 
     public function __construct()
     {
         $this->user = JWTAuth::parseToken()->authenticate();
+        $this->UserService = new UserService($this->user);
     }
 
     public function getUser () {
-        /*
-        *
-        sumPoint = SELECT SUM(point) FROM problems WHERE id IN ( SELECT problem_id FROM ActiveLogs WHERE user_id = $this->user->id )
-
-        SELECT
-            ( sumPoint ) AS sumPoint,
-            ( CEILING( ( sumPoint )/500 ) ) AS Rank ,
-            ( SELECT count(id) FROM activelogs WHERE user_id = $this->user->id ) AS SolveCount ,
-            name
-        FROM
-            users
-        WHERE
-            id = $this->user->id;
-        *
-        */
-        return $request = Problem::selectRaw(
-            'distinct users.name as user, count(*) as solved, sum(problems.point) as point, CEILING(sum(problems.point)/500) AS rank'
-            )
-        ->join('activeLogs','activeLogs.problem_id', '=', 'problems.id')
-        ->join('users','activeLogs.user_id','=','users.id')
-        ->where('users.id',$this->user->id)->groupBy('users.id')->get();
+        return $this->UserService->getInfo();
+    }
+    public function getRatio () {
+        return $this->UserService->getPei();
+    }
+    public function getList () {
+        return $this->UserService->getList();
     }
 
 
