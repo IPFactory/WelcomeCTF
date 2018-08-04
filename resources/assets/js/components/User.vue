46aedf4d40ae2306af59745cae7cbcc4a96e1120
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2018-08-04 15:43:56 +0900

    [add]ユーザーページの暫定修正

--- resources/assets/js/components/User.vue
+++ resources/assets/js/components/User.vue
@@ -6,29 +6,31 @@
                 <img id="icon-image" :src=myIcon>
             </div>
             <div id="user-name">
-                <p>{{ myName }}</p>
+                <p>{{ user['user'] }}</p>
             </div>
             <div >
-                <p id="info"><span>Rank</span><span>:</span><span>{{ myRank }}</span></p>
-                <p id="info"><span>Point</span><span>:</span><span>{{ myPoint }}</span></p>
-                <p id="info"><span>Solved</span><span>:</span><span>{{ mySolved }}</span></p>
+                <p id="info"><span>Rank</span><span>:</span><span>{{ user['rank'] }}</span></p>
+                <p id="info"><span>Point</span><span>:</span><span>{{ user['point'] }}</span></p>
+                <p id="info"><span>Solved</span><span>:</span><span>{{ user['solved'] }}</span></p>
             </div>
         </div>
         <div id="graph">
-            <pie :datas=datas :labels=labels></pie>
+            <pie></pie>
         </div>
         <div id="ploblemTable">
             <table>
                 <caption>solve ploblem</caption>
                 <tr id="table1">
+                    <th id="id">ID</th>
                     <th id="solve">solve</th>
                     <th id="ploblem">問題名</th>
                     <th id="genre">ジャンル</th>
                     <th id="point">得点</th>
                 </tr>
                 <tr v-for="(list, index) in lists" v-bind:id=bindeId+index%2>
+                    <td>{{index + 1}}</td>
                     <td><b v-if=list.isSolve>★</b></td>
-                    <td>{{ list.ploblem }}</td>
+                    <td>{{ list.title }}</td>
                     <td>{{ list.genre }}</td>
                     <td>{{ list.point }}</td>
                 </tr>
@@ -42,38 +44,44 @@
 
 <script>
 import Vue  from "vue";
+import http from "../service/http"
+
 Vue.component('pie', require('./chart/pie.vue'));
 export default {
     props:["show"],
     mounted () {
-        this.isAuth()
+        this.getUserData()
+        this.getProblemList()
     },
     updated () {
         this.isAuth()
     },
     data () {
         return {
+            user    : [],
+            cate    : [],
             bindeId : "table",
             myIcon  : "/image/icon/icon-400.png",
-            myName  : "Test Name",
-            myRank  : 20,
-            myPoint : 2000,
-            mySolved: 6,
-            datas   : [5, 5, 5, 5, 5, 5, 5],
-            labels  : ["misc", "ppc", "crypt", "web", "bin", "pwn", "net"],
-            lists: [
-                {isSolve:1,ploblem: 'りんご',genre: "Web",point: 100},
-                {isSolve:1,ploblem: 'ごりら',genre: "bin",point: 10},
-                {isSolve:1,ploblem: 'らっぱ',genre: "bin",point: 200},
-                {isSolve:0,ploblem: 'ぱんつ',genre: "fre",point: 100},
-                {isSolve:1,ploblem: 'つみき',genre: "pwn",point: 100},
-                {isSolve:0,ploblem: 'きのこ',genre: "Web",point: 100},
-                {isSolve:1,ploblem: 'こーら',genre: "Web",point: 100},
-
-            ]
+            lists   : []
         }
     },
     methods :{
+        getUserData () {
+            http.get("/user/info",res => {
+                this.user = JSON.stringify(res.data[0]);
+                this.user = JSON.parse(this.user)
+            },error => {
+                console.log("err")
+            })
+        },
+        getProblemList () {
+            http.get("user/list",res => {
+                this.lists = JSON.stringify(res.data);
+                this.lists = JSON.parse(this.lists)
+            },error => {
+                console.log("err")
+            })
+        },
         isAuth () {
             if (!this.show) {
                 this.$router.push('/')
