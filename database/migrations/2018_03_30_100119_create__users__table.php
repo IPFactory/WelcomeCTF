b6be89feb0926c3914dc04d201647b2ff163c7c1
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2018-09-30 15:49:03 +0900

    Userに変更

--- database/migrations/2018_03_30_100119_create__users__table.php
+++ database/migrations/2018_03_30_100119_create__users__table.php
@@ -8,7 +8,7 @@ class CreateUsersTable extends Migration
 {
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('Users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name', 255);
             $table->text("email");
