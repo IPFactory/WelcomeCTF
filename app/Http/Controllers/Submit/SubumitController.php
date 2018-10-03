fd6850f96b8720d59624cc493d5c933cc12fa189
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2018-10-03 19:42:40 +0900

    submit用controller

---
+++ app/Http/Controllers/Submit/SubumitController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Subumit;
+
+use JWTAuth;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use App\Http\Controllers\Controller;
+use App\Service\SubumitService as SubumitService;
+
+
+class SubumitController extends Controller
+{
+    private $user;
+    private $SubumitService;
+
+    public function __construct(Request $Request)
+    {
+        try {
+            $this->user = JWTAuth::parseToken()->authenticate();
+            $this->SubumitService = new SubumitService($this->user, $Request);
+        } catch (JWTException $e){
+            return response()->json(['error' => 'Authentication failed'], 401);
+        }
+    }
+
+    public function postSubumit ($probId) {        //サブミット
+
+        if ( $this->flagVerifi($probId) ) {
+            return false;
+        }
+
+        $this->tamperPrevention();
+        $this->SubumitService->submit($probId);
+        $this->regularly();
+
+        return true ;
+    }
+
+    private function flagVerifi () {  //フラグ検証
+        return $this->SubumitService->flagVerifi() ;
+    }
+
+    private function regularly () {         //ポイントの定期更新
+        return $this->SubumitService->regularly() ;
+    }
+
+    private function tamperPrevention () {  //スコアサーバー改ざん防止(暫定)
+        return $this->SubumitService->tamperPrevention() ;
+    }
+
+
+}
