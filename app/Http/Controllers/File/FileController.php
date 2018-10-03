ba17a36833cbb2d92449583d7dc40902349423ae
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2018-10-03 14:17:48 +0900

    fileダウンロード用のcontroller(仮)

---
+++ app/Http/Controllers/File/FileController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\File;
+
+use JWTAuth;
+use Storage;
+use App\Model\User;
+use App\Model\Problem;
+use App\Model\ProblemFile;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use App\Service\ProblemService as ProblemService;
+
+
+class FileController extends Controller
+{
+    private $user;
+    private $request;
+    private $id;
+
+    public function __construct (Request $Request) {
+        try {
+            $this->user           =   JWTAuth::parseToken()->authenticate();
+            $this->request       =    $Request;
+        } catch (JWTException $e){
+            return response()->json(['error' => 'Authentication failed'], 401);
+        }
+    }
+    /*
+    *   Control
+    */
+    public function getProblemFile ($file_name ) {
+
+        $find = ProblemFile::where('first_data', $file_name)
+                ->orWhere('second_data', $file_name)
+                ->get();
+
+        if( !isset($file_name) || !isset($find[0]->id) ){
+            return redirect("/main");
+        }
+
+        return Storage::disk('s3')->download("ctffiles/".$file_name);;
+    }
+}
