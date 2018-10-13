81fca2ec636f1a89ee917ceaa0f0bacd8dedb737
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2018-10-13 09:22:00 +0900

    修正

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Test Vue</title>
+        <title>WelcomeCTF</title>
         <script>
 //        m=["log","debug","warn","error","dir","trace","assert","dirxml","group","groupEnd","time","timeEnd","count","profile","profileEnd","network"];n=1;function z(){window.console={};m.forEach(function(b){(function(a){if(console[a]&&n){window.console[a]=function(d){return}}else{window.console[a]=function(){}}})(b)})}z();
         </script>
@@ -13,7 +13,7 @@
             window.Laravel = {};
             window.Laravel.csrfToken = "{{ csrf_token() }}";
             window.Laravel.message = "{{ Session::get('success') }}";
-            window.problems = []; 
+            window.problems = [];
         </script>
         <link rel="stylesheet" href="{{mix('css/app.css')}}">
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" />
